dc97b148687461bd6df5b105928a07b3bd717d61
Jaeyub Chung <jybchung@berkeley.edu>
2020-10-23 07:04:12 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,10 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    @all_ratings = Movie.all_ratings()
+    @ratings_to_show = Array.new
+    
+    
   end
 
   def new
