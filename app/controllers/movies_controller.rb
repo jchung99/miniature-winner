22700ff67d46b87316b8d476611a6e89a825df64
Jaeyub Chung <jybchung@berkeley.edu>
2020-10-29 03:17:40 +0000

    pls

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -19,9 +19,9 @@ class MoviesController < ApplicationController
       is_home = true
     end
     @ratings_to_show = @all_ratings
-    if session[:ratings] != nil
+    if params[:ratings] == nil
       @ratings_to_show = session[:ratings]
-      @movies = Movie.filter_movies(@ratings_to_show)
+      redirect_to movies_path(:ratings=> @ratings_to_show)
 
     end
 #     if session[:in_session] =="yes" and !is_home
@@ -36,7 +36,7 @@ class MoviesController < ApplicationController
       @click_date = "hilite bg-warning"
     end
     if params["ratings"] != nil
-      @ratings_to_show = params["ratings"].keys()
+      @ratings_to_show = params["ratings"]
       @movies = Movie.filter_movies(@ratings_to_show)
       session[:ratings] = @ratings_to_show
     end
